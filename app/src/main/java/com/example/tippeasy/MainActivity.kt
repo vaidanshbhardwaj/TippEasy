512133d3fe73e56becb04161092cf382f255030b
Vaidansh Bhardwaj <vaidansh.23902@gmail.com>
2021-12-27 05:04:25 +0530

    Improved Design of the app

--- app/src/main/java/com/example/tippeasy/MainActivity.kt
+++ app/src/main/java/com/example/tippeasy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tippeasy
 
+import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -8,6 +9,7 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 private const val TAG = "MainActivity"
 private const val INITIAL_TIP_PERCENT = 15
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvTipAmt: TextView
     private lateinit var tvTotalAmt: TextView
     private lateinit var tvTipPercentLabel: TextView
+    private lateinit var tvTipDescription: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,14 +29,17 @@ class MainActivity : AppCompatActivity() {
         tvTipAmt = findViewById(R.id.tvTipAmt)
         tvTotalAmt = findViewById(R.id.tvTotalAmt)
         tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
 
         seekBarTip.progress = INITIAL_TIP_PERCENT
         tvTipPercentLabel.text = "$INITIAL_TIP_PERCENT%"
+        updateTipDescription(INITIAL_TIP_PERCENT)
         seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 Log.i(TAG, "onProgressChanged $p1")
                 tvTipPercentLabel.text = "$p1%"
                 computeTipAndTotal()
+                updateTipDescription(p1)
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -52,6 +58,24 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+    private fun updateTipDescription(tipPercent: Int) {
+        val tipDescription = when (tipPercent){
+            in 0..9 -> "Poor"
+            in 10..14 ->"Acceptable"
+            in 15..19 ->"Good"
+            in 20..24 ->"Great"
+            else -> "Splendid"
+        }
+        tvTipDescription.text = tipDescription
+//        Updating the color based on the tipPercent
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat()/seekBarTip.max,
+            ContextCompat.getColor(this,R.color.color_worst_tip),
+            ContextCompat.getColor(this,R.color.color_best_tip)
+        ) as Int
+        tvTipDescription.setTextColor(color)
     }
 
     private fun computeTipAndTotal() {
